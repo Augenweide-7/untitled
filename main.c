e9a008e8829ceaad4dc9961de224320812797b22
Augenweide-7 <user242968@163.com>
2021-01-29 01:00:53 +0800

    九九乘法表

--- main.c
+++ main.c
@@ -1,12 +1,10 @@
 #include<stdio.h>
 int main(){
     for(int i=1;i<10;i++){
-        for(int m=1;m<10-i;m++){
-            printf("\t\t");
-        }
-        for(int j=1;j<=i;j++){
+        for(int j=1;j<10;j++){
             printf("%d*%d=%2d  ",i,j,i*j);
         }
+
         printf("\n");
     }
 
