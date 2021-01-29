f8856341b58c929f03ac72ec6168a3f98a91565b
Augenweide-7 <user242968@163.com>
2021-01-29 17:57:45 +0800

    *菱形

--- main.c
+++ main.c
@@ -1,11 +1,29 @@
 #include<stdio.h>
 int main(){
-    for(int i=1;i<10;i++){
-        for(int j=i ;j<10;j++){
-            printf("%d*%d=%2d  ",i,j,i*j);
+    int m;
+    scanf("%d",&m);
+    for(int i=1;i<=m;i++){
+        if(i<=m/2+1){
+            for(int j=1;j<=m;j++){
+                if(j>=m/2-i+2&&j<=m/2+i){
+                    printf("*");
+                }else{
+                    printf(" ");
+                }
+            }printf("\n");
+        }else{
+            for(int j=1;j<m;j++){
+                if(j>=i-m/2&&j<=m-(i-m/2-1)){
+                    printf("*");
+                }else{
+                    printf(" ");
+                }
+            }printf("\n");
+
         }
-        printf("\n");
     }
+
+
 
     return 0;
 }
