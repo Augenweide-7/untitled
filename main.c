e84d59272146c44f572ec66fa3f5cc725fde48cf
Augenweide-7 <user242968@163.com>
2021-01-30 16:35:30 +0800

    pi2

--- main.c
+++ main.c
@@ -1,20 +1,17 @@
 #include<stdio.h>
 #include<math.h>
 int main(){
-    float pi=0.0;
-    float i=1.0;
-    float sign=1.0;
-    float n=1.0;
-    while(fabs(i)>=1e-6){
-        pi+=i;
-        sign=-sign;
-        n+=2;
-        i=sign/n;
-
+    float pi=1.0;
+    float n=1;
+    int i;
+    for(i=1;i<=100;i++,n++){
+        if(i%2==0){
+            pi*=(n/(n+1));
+        }else{
+            pi*=((n+1)/n);
+        }
     }
-    pi=pi*4;
+    pi=pi*2;
     printf("%.6f",pi);
-
-
     return 0;
 }
