742c1a76c8de070b243b875cf894ec38dc563fc1
Augenweide-7 <user242968@163.com>
2021-02-05 11:50:18 +0800

    4.8.7

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
+#include<float.h>
 int main(){
-    char name[40];
-    char surname[40];
-    printf("Please input your firstname:");
-    scanf("%s",name);
-    printf("Please input your lastname:");
-    scanf("%s",surname);
-    printf("%s,%s",name,surname);
+    double d=1.0/3.0;
+    float f=1.0/3.0;
+    printf("%.6lf %.12lf %.16lf\n",d,d,d);
+    printf("%.6f %.12f %.16f\n",f,f,f);
+    printf("FLT_DIG:%d\n",FLT_DIG);
+    printf("DBL_DIG:%d\n",DBL_DIG);
+
     return 0;
 }
