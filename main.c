805b212724715e58c4bfbdbdbdaeadd836cabe8d
Augenweide-7 <user242968@163.com>
2021-02-05 12:33:09 +0800

    4.8.8

--- main.c
+++ main.c
@@ -1,15 +1,13 @@
 #include<stdio.h>
+#define GALLON_TO_LITRE 3.785
+#define MILE_TO_KM 1.609
 int main(){
-    char name[40];
-    char surname[40];
-    printf("Please input your firstname:");
-    scanf("%s",name);
-    printf("Please input your lastname:");
-    scanf("%s",surname);
-    int wname=printf("%s",name);
-    printf(" ");
-    int wsurname=printf("%s",surname);
-    printf("\n");
-    printf("%*d %*d",wname,wname,wsurname,wsurname);
+    float range,oil;
+    printf("Please input the range yuo traveled(in mile):");
+    scanf("%f",&range);
+    printf("Please input the oil you spend(in gallon):");
+    scanf("%f",&oil);
+    printf("%.1f M/G",range/oil);
+    printf("%.1f l/100KM",(oil*GALLON_TO_LITRE)/(range*MILE_TO_KM));
     return 0;
 }
