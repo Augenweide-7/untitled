86026e2d60fcd9e025fb6e4b2b240d1d85dc11c5
Augenweide-7 <user242968@163.com>
2021-02-06 12:39:40 +0800

    5.11.3 day and week

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
+#define DAY_PER_WEEK 7;
 int main(){
-    int num;
-    int i=0;
-    printf("Please input a number:");
-    scanf("%d",&num);
-    while(i++<=10){
-        printf("%d\n",num);
-        num++;
+    int day,week,input;
+    printf("Please input the number of days(<=0 to quit):");
+    scanf("%d",&input);
+    while(input>0){
+        week=input/DAY_PER_WEEK;
+        day=input%DAY_PER_WEEK;
+        printf("%d days are %d weeks,%d days\n",input,week,day);
+        printf("Please input the number of days(<=0 to quit):");
+        scanf("%d",&input);
     }
     printf("Program exit!");
     return 0;
