0ee5eb464caee9c6ba949aa3249a78db94040331
Augenweide-7 <user242968@163.com>
2021-02-07 00:21:31 +0800

    5.11.7 cube

--- main.c
+++ main.c
@@ -1,19 +1,14 @@
 #include<stdio.h>
-#define FEET_TO_CM 30.48
-#define INCH_TO_CM 2.54
+double cube(double num);
 int main(){
-    int feet;
-    float cm,inches;
-    printf("Enter a height in centimeters:");
-    scanf("%f",&cm);
-    while(cm>0){
-        feet=cm/FEET_TO_CM;
-        inches=(cm-feet*FEET_TO_CM)/INCH_TO_CM;
-        printf("%.1f cm = %d feet,%.1f inches\n",cm,feet,inches);
-        printf("Enter a height in centimeters(<=0 to quit):");
-        scanf("%f",&cm);
-
-    }
-    printf("bye");
+    double num;
+    printf("Please input a number:");
+    scanf("%lf",&num);
+    cube(num);
+    printf("Program exit!");
     return 0;
+}
+double cube(double num){
+    double n=num*num*num;
+    printf("%lf\n",n);
 }
