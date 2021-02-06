227b309bf7bd28c1e60f2a7a5411e8e624aec1ad
Augenweide-7 <user242968@163.com>
2021-02-06 12:15:26 +0800

    5.11.2

--- main.c
+++ main.c
@@ -1,17 +1,13 @@
 #include<stdio.h>
-#define MIN_PER_HOUR 60
 int main(){
-    int hour,min,input;
-    printf("Please input the number of minutes(<=0 to quit):");
-    scanf("%d",&input);
-    while(input>0){
-        hour=input/MIN_PER_HOUR;
-        min=input%MIN_PER_HOUR;
-        printf("%d hour and %d minutes\n",hour,min);
-        printf("Please input the number of minutes(<=0 to quit):");
-        scanf("%d",&input);
+    int num;
+    int i=0;
+    printf("Please input a number:");
+    scanf("%d",&num);
+    while(i++<=10){
+        printf("%d\n",num);
+        num++;
     }
     printf("Program exit!");
-
     return 0;
 }
