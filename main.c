b65d1ba99066ec03ca20fc228c2978ca5e1c86ba
Augenweide-7 <user242968@163.com>
2021-02-07 17:44:04 +0800

    5.11.9 F,t,K

--- main.c
+++ main.c
@@ -1,17 +1,25 @@
 #include<stdio.h>
 int main(){
-    int first,second;
-    printf("This program computes moduli.\n");
-    printf("Enter an integer to serve as the second operand:");
-    scanf("%d",&second);
-    printf("Now enter the first operand:");
-    scanf("%d",&first);
-    while(first>0){
-        printf("%d %% %d is %d\n",first,second,first%second);
-        printf("Enter next number for first operand(<=0 to quit):");
-        scanf("%d",&first);
-
+    double input;
+    int Temperatures(double fahrenheit);
+    printf("Please input the degree fahrenheit:");
+    int status;
+    status=scanf("%lf",&input);
+    while(status==1){
+        Temperatures(input);
+        printf("Please input the next degree fahrenheit(q to quit):");
+        status=scanf("%lf",&input);
     }
-    printf("Done");
+    printf("Program exit!");
     return 0;
+}
+int Temperatures(double fahrenheit){
+    const double F_TO_C=32.0;
+    const double C_TO_K=273.16;
+    double celsius,kelvin;
+    celsius=5.0/9.0*(fahrenheit-F_TO_C);
+    kelvin=celsius+C_TO_K;
+    printf("F=%.2lf t=%.2lf K=%.2f\n",fahrenheit,celsius,kelvin);
+    return 0;
+
 }
