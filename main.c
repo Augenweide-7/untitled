8e24b766ec6de686311226515003f9bb7643c613
Augenweide-7 <user242968@163.com>
2021-02-13 20:50:37 +0800

    6.16.1 a b c d...

--- main.c
+++ main.c
@@ -1,25 +1,11 @@
 #include<stdio.h>
 int main(){
-    double input;
-    int Temperatures(double fahrenheit);
-    printf("Please input the degree fahrenheit:");
-    int status;
-    status=scanf("%lf",&input);
-    while(status==1){
-        Temperatures(input);
-        printf("Please input the next degree fahrenheit(q to quit):");
-        status=scanf("%lf",&input);
+    char str[26];
+    char num='a';
+    for(int i=0;i<26;i++){
+        str[i]=num;
+        num++;
+        printf("%c ",str[i]);
     }
-    printf("Program exit!");
     return 0;
-}
-int Temperatures(double fahrenheit){
-    const double F_TO_C=32.0;
-    const double C_TO_K=273.16;
-    double celsius,kelvin;
-    celsius=5.0/9.0*(fahrenheit-F_TO_C);
-    kelvin=celsius+C_TO_K;
-    printf("F=%.2lf t=%.2lf K=%.2f\n",fahrenheit,celsius,kelvin);
-    return 0;
-
 }
