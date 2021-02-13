fbcfef778c39ea62067236bf9858b2237f0a8c68
Augenweide-7 <user242968@163.com>
2021-02-13 20:53:17 +0800

    6.16.2

--- main.c
+++ main.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 int main(){
-    char str[26];
-    char num='a';
-    for(int i=0;i<26;i++){
-        str[i]=num;
-        num++;
-        printf("%c ",str[i]);
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=i;j++){
+            printf("$");
+        }
+        printf("\n");
     }
     return 0;
 }
