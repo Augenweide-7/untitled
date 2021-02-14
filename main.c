2788583c1ba7f9f326e63fc84a8ac509857201f9
Augenweide-7 <user242968@163.com>
2021-02-14 20:26:48 +0800

    6.16.3

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 int main(){
-    for(int i=1;i<=5;i++){
-        for(int j=1;j<=i;j++){
-            printf("$");
+    for(int i=1;i<=6;i++){
+        char num='F';
+        for(int j=1;j<=i;j++,num--){
+            printf("%c",num);
         }
         printf("\n");
     }
+
     return 0;
 }
