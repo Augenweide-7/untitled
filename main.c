fa8f15693f146e7adb73e368968a619932060ab2
Augenweide-7 <user242968@163.com>
2021-02-16 21:57:56 +0800

    input score

--- main.c
+++ main.c
@@ -1,12 +1,20 @@
 #include<stdio.h>
 int main(){
-    char num='A';
-    for(int i=1;i<=6;i++){
-        for(int j=1;j<=i;j++,num++){
-            printf("%c",num);
+    int arr[5][3];
+    int aver[3];
+    int sum;
+    int average;
+    printf("Please input the score:");
+    for(int i=0;i<3;i++){
+        for(int j=0;j<5;j++){
+            scanf("%d",&arr[i][j]);
+            sum+=arr[i][j];
         }
-        printf("\n");
+        aver[i]=sum/5;
+        sum=0;
     }
-
+    average=(aver[0]+aver[1]+aver[2])/3;
+    printf("Math:%d\nLanguage:%d\nEnglish:%d\n ",aver[0],aver[1],aver[2]);
+    printf("Average:%d",average);
     return 0;
 }
