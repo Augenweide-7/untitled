8fcab7c4f23d8eb1329a3043c3280039bfbf7e4d
Augenweide-7 <user242968@163.com>
2021-03-03 21:26:41 +0800

    一尺之棰

--- main.c
+++ main.c
@@ -1,24 +1,13 @@
 #include<stdio.h>
+#include<math.h>
 int main(){
-    int n,k;
-    int sum1=0,sum2=0;
-    float aver1,aver2;
-    float num1=0.0,num2=0.0;
-    scanf("%d %d",&n,&k);
-    for(int i=1;i<=n;i++){
-        if(i%k==0){
-            sum1+=i;
-            num1++;
-        }else{
-            sum2+=i;
-            num2++;
-        }
-
+    int a;
+    scanf("%d",&a);
+    int day=1;
+    for(;a!=1;){
+        day++;
+        a=floor(a/2);
     }
-    aver1=sum1/num1;
-    aver2=sum2/num2;
-    printf("%.1f %.1f",aver1,aver2);
-
-
+    printf("%d",day);
     return 0;
 }
