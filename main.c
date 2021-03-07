d1fc7efa64feb7f6123a167f630bc02b4bf1c175
Augenweide-7 <user242968@163.com>
2021-03-07 19:45:09 +0800

    数列求和

--- main.c
+++ main.c
@@ -1,15 +1,11 @@
 #include<stdio.h>
 int main(){
     int n;
+    int sum=0;
     scanf("%d",&n);
-    int t=0;
     for(int i=1;i<=n;i++){
-        for(int j=1;j<=n-i+1;j++){
-            t++;
-            printf("%02d",t);
-            }
-        printf("\n");
-        }
-
+        sum+=i;
+    }
+    printf("%d",sum);
     return 0;
 }
