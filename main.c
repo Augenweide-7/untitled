76cd9749635d7ad3bcd67022781de56dc1788283
Augenweide-7 <user242968@163.com>
2021-03-07 20:39:27 +0800

    P5724求极差

--- main.c
+++ main.c
@@ -1,12 +1,26 @@
 #include<stdio.h>
+#include<stdbool.h>
 int main(){
-    float s,n,sum=2;
-    scanf("%f",&s);
-    int loop=1;
-    for(n=2;sum<s;loop++){
-        n=n*0.98;
-        sum+=n;
+    int n,sum=0;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
     }
-    printf("%d",loop);
+    int temp;
+    bool flag=true;
+    for(int i=1;i<=n-1&&flag;i++){
+        for(int j=1;j<=n-i;j++){
+            flag=false;
+            if(a[j-1]>a[j]){
+                flag=true;
+                temp=a[j-1];
+                a[j-1]=a[j];
+                a[j]=temp;
+
+            }
+        }
+    }
+    printf("%d",a[n-1]-a[0]);
     return 0;
 }
