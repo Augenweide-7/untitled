ab279bb9edfcd279f96cc73b6c14913d361317c6
Augenweide-7 <user242968@163.com>
2021-03-07 18:22:23 +0800

    数字直角三角形

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
-#include<math.h>
 int main(){
-    int a;
-    scanf("%d",&a);
-    int day=1;
-    for(;a!=1;){
-        day++;
-        a=floor(a/2);
-    }
-    printf("%d",day);
+    int n;
+    scanf("%d",&n);
+    int t=0;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i+1;j++){
+            t++;
+            printf("%02d",t);
+            }
+        printf("\n");
+        }
+
     return 0;
 }
