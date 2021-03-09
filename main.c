c66ce438a808f5187bc4f0450dcaaf49762f249f
Augenweide-7 <user242968@163.com>
2021-03-09 20:40:05 +0800

    P1980 计数问题

--- main.c
+++ main.c
@@ -1,21 +1,19 @@
 #include<stdio.h>
 int main(){
-    int K;
-    scanf("%d",&K);
-    int days=0,coin=0;
-    for(int i=1;i<=K;i++){
-        for(int j=1;j<=i;j++){
-            coin+=i;
-            days+=1;
-            if(days==K){
-                break;
+    int n,x;
+    scanf("%d %d",&n,&x);
+    int times=0;
+    for(int i=1;i<=n;i++){
+        int temp=i;
+        int c;
+        while(temp>0){
+            c=temp%10;
+            temp/=10;
+            if(c==x){
+                times++;
             }
         }
-        if(days==K){
-            break;
-        }
     }
-    printf("%d",coin);
-
+    printf("%d",times);
     return 0;
 }
