1b94128b4adcbac42a5041fd814e2b32adb18af9
Augenweide-7 <user242968@163.com>
2021-03-09 17:38:17 +0800

    P1035 级数求和

--- main.c
+++ main.c
@@ -1,26 +1,25 @@
 #include<stdio.h>
 #include<stdbool.h>
 int main(){
-    int n,sum=0;
-    scanf("%d",&n);
-    int a[n];
-    for(int i=0;i<n;i++){
-        scanf("%d",&a[i]);
-    }
-    int temp;
     bool flag=true;
-    for(int i=1;i<=n-1&&flag;i++){
-        for(int j=1;j<=n-i;j++){
-            flag=false;
-            if(a[j-1]>a[j]){
+    int k,n=1;
+    scanf("%d",&k);
+    double num,Sn=0.0;
+    if(k==0){
+        printf("0");
+    }else{
+        for(int i=1;flag;i++){
+            num=1.0/i;
+            Sn+=num;
+            if(Sn<=k){
                 flag=true;
-                temp=a[j-1];
-                a[j-1]=a[j];
-                a[j]=temp;
-
+                n++;
+            }else{
+                flag=false;
             }
+
         }
+        printf("%d",n);
     }
-    printf("%d",a[n-1]-a[0]);
     return 0;
 }
