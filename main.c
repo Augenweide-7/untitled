651cf0fe8d9a0a68c4e41649bbace970e63fac4b
Augenweide-7 <user242968@163.com>
2021-03-10 20:30:30 +0800

    P4414 ABC switch case

--- main.c
+++ main.c
@@ -1,19 +1,34 @@
 #include<stdio.h>
 int main(){
-    int n,x;
-    scanf("%d %d",&n,&x);
-    int times=0;
-    for(int i=1;i<=n;i++){
-        int temp=i;
-        int c;
-        while(temp>0){
-            c=temp%10;
-            temp/=10;
-            if(c==x){
-                times++;
+    int a[3];
+    int temp;
+    for(int i=0;i<3;i++){
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<3-1;i++){
+        for(int j=0;j<3-i-1;j++){
+            if(a[j]>a[j+1]){
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
             }
         }
     }
-    printf("%d",times);
+    char str[4];
+    scanf("%s",&str);
+    for(int i=0;str[i]!=0;i++){
+        switch(str[i]){
+            case 'A':
+                printf("%d ",a[0]);
+                break;
+            case 'B':
+                printf("%d ",a[1]);
+                break;
+            case 'C':
+                printf("%d ",a[2]);
+                break;
+        }
+    }
+
     return 0;
 }
