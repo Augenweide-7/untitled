f2f76b099380a3361ac2b8fbd8ffbaf3cb844c5e
Augenweide-7 <user242968@163.com>
2021-03-10 20:54:50 +0800

    P1422 小玉家的电费

--- main.c
+++ main.c
@@ -1,34 +1,15 @@
 #include<stdio.h>
 int main(){
-    int a[3];
-    int temp;
-    for(int i=0;i<3;i++){
-        scanf("%d",&a[i]);
+    int sum;
+    scanf("%d",&sum);
+    float cost;
+    if(sum<=150){
+        cost=sum*0.4463;
+    }else if(sum<=400){
+        cost=150*0.4463+(sum-150)*0.4663;
+    }else{
+        cost=150*0.4463+(400-150)*0.4663+(sum-400)*0.5663;
     }
-    for(int i=0;i<3-1;i++){
-        for(int j=0;j<3-i-1;j++){
-            if(a[j]>a[j+1]){
-                temp=a[j];
-                a[j]=a[j+1];
-                a[j+1]=temp;
-            }
-        }
-    }
-    char str[4];
-    scanf("%s",&str);
-    for(int i=0;str[i]!=0;i++){
-        switch(str[i]){
-            case 'A':
-                printf("%d ",a[0]);
-                break;
-            case 'B':
-                printf("%d ",a[1]);
-                break;
-            case 'C':
-                printf("%d ",a[2]);
-                break;
-        }
-    }
-
+    printf("%.1f",cost);
     return 0;
 }
