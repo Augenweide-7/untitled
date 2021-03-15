25c301fce0754edc4ff5a37c99796513559db28f
Augenweide-7 <user242968@163.com>
2021-03-15 20:57:01 +0800

    P1888 三角函数

--- main.c
+++ main.c
@@ -1,19 +1,27 @@
 #include<stdio.h>
-int min(int x,int y){
-    if(x>y)
-        return y;
-    else
-        return x;
-}
 int main() {
-    int fz,fm;
-    scanf("%d/%d",&fz,&fm);
-    for(int i=1;i<min(fz,fm);i++){
-        if(fz%i==0&&fm%i==0){
-            fz/=i;
-            fm/=i;
+    int arr[3];
+    int temp;
+    for(int i=0;i<3;i++){
+        scanf("%d",&arr[i]);//读取三个勾股数
+    }
+    for(int i=0;i<3-1;i++){
+        for(int j=0;j<3-1-i;j++){
+            if(arr[j]>arr[j+1]){
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;//将三个勾股数排序
+            }
         }
     }
-    printf("%d/%d",fz,fm);
+    int a=arr[0],b=arr[1],c=arr[2];
+    int sin=a/c;//定义较小锐角的正弦值
+    for(int i=1;i<a;i++){
+        if(a%i==0&&b%i==0){
+            a/=i;
+            c/=i;
+        }
+    }//约分
+    printf("%d/%d",a,c);//输出较小锐角的正弦值
     return 0;
 }
