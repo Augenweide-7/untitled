5519e4f480f626cfa6a0fda0651ceab497e2ed99
Augenweide-7 <user242968@163.com>
2021-03-15 20:43:13 +0800

    约分

--- main.c
+++ main.c
@@ -1,18 +1,19 @@
 #include<stdio.h>
+int min(int x,int y){
+    if(x>y)
+        return y;
+    else
+        return x;
+}
 int main() {
-    int a[10];
-    int height;
-    int h=30;
-    for (int i = 0; i < 10; i++) {
-        scanf("%d", &a[i]);//读取10个苹果到地面的高度
-    }
-    scanf("%d", &height);//读取手伸直时能达到的高度
-    int loop=0;
-    for (int i = 0; i < 10; i++) {
-        if (a[i]<=height+h){
-            loop++;
+    int fz,fm;
+    scanf("%d/%d",&fz,&fm);
+    for(int i=1;i<min(fz,fm);i++){
+        if(fz%i==0&&fm%i==0){
+            fz/=i;
+            fm/=i;
         }
     }
-    printf("%d",loop);
+    printf("%d/%d",fz,fm);
     return 0;
 }
