69608be13e0a3c6e669820d28940e6fc69e1038c
Augenweide-7 <user242968@163.com>
2021-03-15 19:39:27 +0800

    P5717 三角形分类（将三条边进行排序）

--- main.c
+++ main.c
@@ -1,15 +1,39 @@
 #include<stdio.h>
 int main(){
-    int sum;
-    scanf("%d",&sum);
-    float cost;
-    if(sum<=150){
-        cost=sum*0.4463;
-    }else if(sum<=400){
-        cost=150*0.4463+(sum-150)*0.4663;
-    }else{
-        cost=150*0.4463+(400-150)*0.4663+(sum-400)*0.5663;
+    int arr[3];
+    int temp;
+    for(int i=0;i<3;i++){
+        scanf("%d",&arr[i]);
     }
-    printf("%.1f",cost);
+    for(int i=0;i<3-1;i++){
+        for(int j=0;j<3-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+
+            }
+        }
+    }
+    int a=arr[0],b=arr[1],c=arr[2];
+    if(a+b>c){
+        if(a*a+b*b==c*c){
+            printf("Right triangle\n");
+        }else if(a*a+b*b>c*c){
+            printf("Acute triangle\n");
+        }else{
+            printf("Obtuse triangle\n");
+        }
+        if(a==b||b==c||a==c){
+            printf("Isosceles triangle\n");
+        }
+        if(a==b&&b==c){
+            printf("Equilateral triangle\n");
+        }
+    }else{
+        printf("Not triangle\n");
+    }
     return 0;
 }
+
+
