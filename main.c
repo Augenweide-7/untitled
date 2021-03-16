dfc993ad672c152eca019e1a2fdb89a88f5be4bb
Augenweide-7 <user242968@163.com>
2021-03-16 23:36:34 +0800

    P1055 ISBN号码

--- main.c
+++ main.c
@@ -1,22 +1,19 @@
 #include<stdio.h>
-#include<math.h>
 int main(){
-    int n;
-    scanf("%d",&n);//读取需要的铅笔数量
-    double num1,num2,num3;
-    int price1,price2,price3;
-    int fee1,fee2,fee3;
-    int fee;
-    scanf("%lf %d\n%lf %d\n%lf %d",&num1,&price1,&num2,&price2,&num3,&price3);//读取三种包装的数量和价格
-    fee1=ceil(n/num1)*price1;
-    fee2=ceil(n/num2)*price2;
-    fee3=ceil(n/num3)*price3;//计算三种包装的价格
-    if(fee1<fee2&&fee1<fee3)
-        fee=fee1;
-    if(fee2<fee1&&fee2<fee3)
-        fee=fee2;
-    if(fee3<fee2&&fee3<fee1)
-        fee=fee3;//比较三种包装价格最低的一种
-    printf("%d",fee);//输出最终价格
+    char a[14],mod[12]="0123456789X";
+    gets(a);//输入字符串
+    int j=1,t=0;
+    for(int i=0;i<12;i++){
+        if(a[i]=='-')
+            continue;
+        else
+            t+=(a[i]-'0')*j++;//计算识别码
+    }
+    if(mod[t%11]==a[12])//判断识别码是否正确
+        printf("Right");
+    else{
+        a[12]=mod[t%11];
+        puts(a);
+    }
     return 0;
 }
