d7a224472d9845434204cd2705605e6983225570
Augenweide-7 <user242968@163.com>
2021-03-16 20:07:39 +0800

    P1909 买铅笔

--- main.c
+++ main.c
@@ -1,27 +1,22 @@
 #include<stdio.h>
-int main() {
-    int arr[3];
-    int temp;
-    for(int i=0;i<3;i++){
-        scanf("%d",&arr[i]);//读取三个勾股数
-    }
-    for(int i=0;i<3-1;i++){
-        for(int j=0;j<3-1-i;j++){
-            if(arr[j]>arr[j+1]){
-                temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;//将三个勾股数排序
-            }
-        }
-    }
-    int a=arr[0],b=arr[1],c=arr[2];
-    int sin=a/c;//定义较小锐角的正弦值
-    for(int i=1;i<a;i++){
-        if(a%i==0&&b%i==0){
-            a/=i;
-            c/=i;
-        }
-    }//约分
-    printf("%d/%d",a,c);//输出较小锐角的正弦值
+#include<math.h>
+int main(){
+    int n;
+    scanf("%d",&n);//读取需要的铅笔数量
+    double num1,num2,num3;
+    int price1,price2,price3;
+    int fee1,fee2,fee3;
+    int fee;
+    scanf("%lf %d\n%lf %d\n%lf %d",&num1,&price1,&num2,&price2,&num3,&price3);//读取三种包装的数量和价格
+    fee1=ceil(n/num1)*price1;
+    fee2=ceil(n/num2)*price2;
+    fee3=ceil(n/num3)*price3;//计算三种包装的价格
+    if(fee1<fee2&&fee1<fee3)
+        fee=fee1;
+    if(fee2<fee1&&fee2<fee3)
+        fee=fee2;
+    if(fee3<fee2&&fee3<fee1)
+        fee=fee3;//比较三种包装价格最低的一种
+    printf("%d",fee);//输出最终价格
     return 0;
 }
