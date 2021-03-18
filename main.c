0b01b83ce2f748db35e5486f5ac8d167ccdc67e2
Augenweide-7 <user242968@163.com>
2021-03-18 17:26:31 +0800

    P1085 不高兴的津津

--- main.c
+++ main.c
@@ -1,19 +1,15 @@
 #include<stdio.h>
 int main(){
-    char a[14],mod[12]="0123456789X";
-    gets(a);//输入字符串
-    int j=1,t=0;
-    for(int i=0;i<12;i++){
-        if(a[i]=='-')
-            continue;
-        else
-            t+=(a[i]-'0')*j++;//计算识别码
+    int max=0,sum,day;
+    int a,b;
+    for(int i=1;i<8;i++){
+        scanf("%d %d",&a,&b);
+        sum=a+b;
+        if(sum>max&&sum>8){
+            day=i;
+            max=sum;
+        }
     }
-    if(mod[t%11]==a[12])//判断识别码是否正确
-        printf("Right");
-    else{
-        a[12]=mod[t%11];
-        puts(a);
-    }
+    printf("%d",day);
     return 0;
 }
