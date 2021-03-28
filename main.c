71d44d2e0a53d9d7d78c7ac7eed619e3c7765eef
Augenweide-7 <user242968@163.com>
2021-03-28 16:26:00 +0800

    my_chmod.c

--- main.c
+++ main.c
@@ -1,21 +1,31 @@
 #include<stdio.h>
-int main(){
-    long long int N;
-    scanf("%lld",&N);
-    if(N==0){
-        printf("0");
-    }else if(N<0){
-        N=-N;
-        printf("-");
+#include<stdlib.h>
+#include<sys/stat.h>
+int main(int argc,char ** argv){
+    int mode;
+    int mode_u;
+    int mode_g;
+    int mode_o;
+    char *path;
+    //检查参数个数的合法性
+    if(argc<3){
+        printf("%s <mode num> <target file>\n",argv[0]);
+        exit(0);
     }
-    int b,flag=0;
-    while(N!=0){
-        b=N%10;
-        N=N/10;
-        if(b!=0||flag){
-            printf("%d",b);
-            flag=1;
-        }
+    //获取命令行参数
+    mode=atoi(argv[1]);//将字符串转换为整数
+    if(mode>777||mode<0){
+        printf("mode num error!\n");
+        exit(0);
+    }
+    mode_u=mode/100;
+    mode_g=(mode-(mode_u*100)/10);
+    mode_o=mode-(mode_u*100)-(mode_g*10);
+    mode=(mode_u*8*8)+(mode_g*8)+mode_o;//将八进制数转换为十进制数
+    path=argv[2];
+    if(chmod(path,mode)==-1){
+        perror("chmod error");
+        exit(1);
     }
     return 0;
 }
