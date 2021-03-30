7a41fa99d2cb259654f17785df0069aa2b3a4ee1
Augenweide-7 <user242968@163.com>
2021-03-30 11:20:06 +0800

    my_rwl.c

--- main.c
+++ main.c
@@ -1,31 +1,20 @@
 #include<stdio.h>
-#include<stdlib.h>
+#include<sys/types.h>
 #include<sys/stat.h>
-int main(int argc,char ** argv){
-    int mode;
-    int mode_u;
-    int mode_g;
-    int mode_o;
-    char *path;
-    //检查参数个数的合法性
-    if(argc<3){
-        printf("%s <mode num> <target file>\n",argv[0]);
-        exit(0);
+#include<fcntl.h>
+#include<unistd.h>
+int main(int argc,char * argv){//测试文件大小
+    int fd=-1;//文件描述符
+    int ret=-1;
+    fd=open("test3.c",O_RDONLY);
+    if(fd<0){
+        perror("open");
     }
-    //获取命令行参数
-    mode=atoi(argv[1]);//将字符串转换为整数
-    if(mode>777||mode<0){
-        printf("mode num error!\n");
-        exit(0);
+    ret=lseek(fd,0,SEEK_END);//将文件指针移动到文件尾，返回相对于文件头的偏移量
+    if(ret<0){
+        perror("lseek");
     }
-    mode_u=mode/100;
-    mode_g=(mode-(mode_u*100)/10);
-    mode_o=mode-(mode_u*100)-(mode_g*10);
-    mode=(mode_u*8*8)+(mode_g*8)+mode_o;//将八进制数转换为十进制数
-    path=argv[2];
-    if(chmod(path,mode)==-1){
-        perror("chmod error");
-        exit(1);
-    }
+    printf("ret=%d\n",ret);
+    close(fd);
     return 0;
 }
