3efc1eafee9730d61827ddeefb07873878dc9e10
Augenweide-7 <user242968@163.com>
2021-04-07 20:19:22 +0800

    链表删除元素

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
-//链表插入元素
+//链表删除元素
 #include<stdio.h>
 #include<stdlib.h>
 typedef struct Link{
     int elem;
-    struct Link *next;
+    struct Link*next;
 }link;
 link *initLink(){
     link *p=NULL;
@@ -14,38 +14,37 @@ link *initLink(){
         a->elem =i;
         a->next =NULL;
         temp->next =a;
-        temp=temp->next ;
+        temp=a;
     }
     return p;
 }
 void displayLink(link *p){
-    link *temp=p;
-    while(temp->next ){
+    link*temp=p;
+    while(temp->next){
         temp=temp->next ;
         printf("%d ",temp->elem );
     }
     printf("\n");
 }
-link *insertLink(link *p,int elem,int add){
-    link *temp=p;//创建一个临时节点
-    //找到要插入位置上的一个节点
+link *delElem(link *p,int add){
+    link *temp=p;
     for(int i=1;i<add;i++){
         temp=temp->next ;
-        if(temp==NULL){
-            printf("插入位置无效");
+        if(temp->next ==NULL){
+            printf("没有该节点\n");
             return p;
         }
     }
-    link *c=(link*)malloc(sizeof(link*));//创建插入节点c
-    c->next =temp->next ;
-    temp->next =c;
+    link *del=temp->next ;//单独设置一个指针指向被删除的节点
+    temp->next =temp->next->next ;
+    free(del);//手动释放被删除的节点
     return p;
 }
 int main(){
     printf("初始化链表为:\n");
     link *p=initLink();
     displayLink(p);
-    insertLink(p,6,3);
+    delElem(p,2);
     displayLink(p);
     return 0;
 }
