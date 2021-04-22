f8424b99e3bfae040a975818c6940a9c04da7d37
Augenweide-7 <user242968@163.com>
2021-04-22 19:33:16 +0800

    pta 打印空心字符菱形

--- main.c
+++ main.c
@@ -1,16 +1,39 @@
 #include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-int main(int argc,char *argv[]){
-    int fd[2];
-    char buf[80];
-    if(pipe(fd)==-1){
-        perror("pipe");
-        exit(1);
+int main(){
+    char c;
+    int n;
+    int i,j;
+    scanf("%c %d",&c,&n);
+    for(i=1;i<=n/2+1;i++){
+        for(j=n/2;j>=i;j--){
+            printf(" ");
+        }
+        for(j=0;j<i*2-1;j++){
+            if(j==0||j==i*2-2){
+                printf("%c",c);
+            }
+            else{
+                printf(" ");
+            }
+        }
+        c+=1;
+        printf("\n");
     }
-    printf("The pipe will read from %d,write to %d.\n",fd[0],fd[1]);
-    write(fd[1],"This is write to pipe!\n",23);
-    read(fd[0],buf,23);
-    printf("%s",buf);
+    c-=1;
+    for(i=1;i<=n/2;i++){
+        c-=1;
+        for(j=1;j<=i;j++){
+            printf(" ");
+        }
+        for(j=n-2;j>=i*2-1;j--){
+            if(j==n-2||j==i*2-1){
+                printf("%c",c);
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
     return 0;
 }
